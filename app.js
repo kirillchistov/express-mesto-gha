ff86de67709fd2a3d16a0d98aab3a55d68a11cee
Kirill Chistov <kirill.v.chistov@yandex.ru>
2022-10-14 20:35:21 +0300

    PR13 0.2 app started in basic config

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.listen(PORT, () => {
-  console.log('Все ОК');
+  console.log(`App successfully started and listening to port ${PORT}`);
 });
