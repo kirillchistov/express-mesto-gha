055e92162eaa7ca802dba3565b453940c2524b81
Kirill Chistov <kirill.v.chistov@yandex.ru>
2022-11-03 02:03:23 +0300

    PR14 0.99 fixing avatar link validation

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (v) => validator.isURL(v),
+      validator: (v) => validator.isURL(v, { protocols: ['http', 'https'], require_protocol: true }),
       message: 'Некорректный адрес ссылки для аватара',
     },
   },
